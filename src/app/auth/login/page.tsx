3861cfe2cfef2da0ba79e408255297336408e219
edureyalabs <edu.reyalabs@gmail.com>
2025-09-30 22:45:18 +0530

    second attempt

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -85,7 +85,7 @@ export default function LoginPage() {
           </Link>
           <div>
             <Link href="/auth/signup" className="text-sm text-blue-600 hover:underline">
-              Don't have an account? Sign up
+              Don&apos;t have an account? Sign up
             </Link>
           </div>
         </div>
